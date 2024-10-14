683cb86af44aed66b09cce73ff0c5c758c2f5303
xamgore <xamgore@ya.ru>
2024-10-14 20:17:41 +0300

    Make serde optional

--- src/lib.rs
+++ src/lib.rs
@@ -20,13 +20,13 @@ pub use docx_template::{CantRenderError, DocxTemplate};
 #[doc(inline)]
 #[cfg(feature = "docx-rust")]
 pub use markup_node::MarkupNode;
-use thiserror::Error;
 #[doc(hidden)]
 pub use transformers::find_and_replace::FindAndReplace;
 #[doc(inline)]
 pub use transformers::find_and_replace::{Placeholders, Replacements, Value};
 
+#[cfg(feature = "serde")]
 #[allow(missing_docs)]
-#[derive(Error, Debug)]
+#[derive(thiserror::Error, Debug)]
 #[error(transparent)]
 pub struct CantSerializeError(#[from] serde_json::Error);
